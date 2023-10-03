63c86d98659dade211e220ac711c95d9dc81144d
Yago <yago_nitto@hotmail.com>
2023-10-03 18:47:25 -0300

    Projeto Final

---
+++ EmprestimoLivro/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmprestimoLivro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivro.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+    }
+}
---
+++ EmprestimoLivro/Models/EmprestimosModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivro.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do recebedor")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do forncedor")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do livro")]
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
